e1f639cadb9c8aca074be8256bf3c0e008cb4a08
kanagano <glintakenoko_mxet_avis@yahoo.co.jp>
2020-03-28 17:27:14 +0900

    made signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,17 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find_by(id: params[:id])
+  end
   def new
     @user = User.new
+  end
+  def create 
+    @user = User.create(name: params[:name], email: params[:email])
+    if @user.save
+      flash[:notice] = "Sign up completed"
+      redirect_to("/users/#{@user.id}")
+    else
+      render("users/new")
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get '/signup', to: 'users#new'
+  get 'signup' => 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post 'users/create' => 'users#create'
+  get 'users/:id' => 'users#show'
 end
