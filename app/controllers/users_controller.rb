224223fdc4ebb8688fdf6309b95ab9f0b6f8ca8d
kanagano <glintakenoko_mxet_avis@yahoo.co.jp>
2020-04-06 18:13:37 +0900

    google maps api successed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,33 @@
 class UsersController < ApplicationController
+  def edit
+    @user = User.find_by(id: params[:id])
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    @user.name = params[:name]
+    @user.email = params[:email]
+    @user.save
+    redirect_to("/users/index")
+  end
+
+  def index
+    @users = User.all
+  end
+  
   def show
     @user = User.find_by(id: params[:id])
   end
+  
   def new
     @user = User.new
   end
+  
   def create 
     @user = User.create(name: params[:name], email: params[:email])
     if @user.save
       flash[:notice] = "Sign up completed"
-      redirect_to("/users/#{@user.id}")
+      redirect_to("/users/index")
     else
       render("users/new")
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  get 'signup' => 'users#new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'maps/index'
+  get '/' => 'home#top'
+  get 'about' => 'home#about'
+  
+  get 'users/:id/edit' => 'users#edit'
+  post 'users/:id/update' => 'users#update'
   post 'users/create' => 'users#create'
+  get 'signup' => 'users#new'
+  get 'users/index' => 'users#index'
   get 'users/:id' => 'users#show'
+
+  resources :maps, only: [:index]
 end
