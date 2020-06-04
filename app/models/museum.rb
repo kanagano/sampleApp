dc0cffe0d00647c2acf5ad5d4515d48019071462
kanagano <glintakenoko_mxet_avis@yahoo.co.jp>
2020-06-04 18:46:11 +0900

    integrated museum image size with scss

--- app/models/museum.rb
+++ app/models/museum.rb
@@ -4,4 +4,10 @@ class Museum < ApplicationRecord
     
     validates :name, {presence: true}
     validates :location, {presence: true}
+    validates :introduction, {presence: true}
+    validates :image_name, {presence: true}
+
+    attribute :location, :string, default: 'undefined'
+    attribute :introduction, :text, default: 'There is no information.'
+    attribute :image_name, :string, default: 'default_image.jpg'
 end
