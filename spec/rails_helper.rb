ba0cb812900059287af95b19a08782aced26c977
kanagano <glintakenoko_mxet_avis@yahoo.co.jp>
2020-06-29 01:00:32 +0900

    fixed codes rubocop pointed out 2

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -23,7 +23,6 @@ require 'capybara/rspec'
 # require only the support files necessary.
 #
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
-require "support"
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
