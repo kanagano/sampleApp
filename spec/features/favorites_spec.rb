90444122ac4820acdbaa4478397d7d3dbb85d1f6
kanagano <glintakenoko_mxet_avis@yahoo.co.jp>
2020-07-01 23:42:48 +0900

    added RSpec codes 20

--- spec/features/favorites_spec.rb
+++ spec/features/favorites_spec.rb
@@ -6,7 +6,7 @@ RSpec.feature "Favorites", type: :feature do
     @museum = FactoryBot.create(:museum)
   end
 
-  scenario "user favors and unfavors a museum" do
+  scenario "user favors and unfavors a museum and sees favorited museums" do
     log_in_as @user
     visit "/museums"
     click_link @museum.name
@@ -15,9 +15,19 @@ RSpec.feature "Favorites", type: :feature do
       click_link "Favorite"
     }.to change {Favorite.count}.by(1)
 
+    visit "/users/#{@user.id}/favorites"
+
+    expect(page).to have_content @museum.name
+
+    visit "/museums/#{@museum.id}"
+
     expect {
       click_link "Unfavorite"
     }.to change {Favorite.count}.by(-1)
+
+    visit "/users/#{@user.id}/favorites"
+
+    expect(page).to have_content "お気に入りの美術館はまだありません"
   end
 
   scenario "it is impossible to favor a museum without login" do
