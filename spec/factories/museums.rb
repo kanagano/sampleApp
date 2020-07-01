7b21cb94c5bd1212e0fec179fe74e2cd4fa760cc
kanagano <glintakenoko_mxet_avis@yahoo.co.jp>
2020-07-01 14:27:23 +0900

    added RSpec codes 9

--- spec/factories/museums.rb
+++ spec/factories/museums.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :museum, aliases: [:target] do
-    name "信濃美術館"
+    sequence(:name) { |n| "Museum no.#{n}" }
     location "長野県長野市箱清水１丁目４−４"
     introduction "長野県信濃美術館・東山魁夷館は、長野県長野市にある県立美術館。"
     image_name "shinano.jpg"
