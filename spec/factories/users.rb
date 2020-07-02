3c14787daabd215a339691442ce77156edc51dae
kanagano <glintakenoko_mxet_avis@yahoo.co.jp>
2020-07-02 11:52:42 +0900

    added RSpec codes 22

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user, aliases: [:writer, :owner, :visitor, :visited] do
-    name "Landy"
+    sequence(:name) { |n| "user no.#{n}" }
     sequence(:email) { |n| "test#{n}@example.com" }
     sequence(:password) { |n| "test#{n}password" }
   end
